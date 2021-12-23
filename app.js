bd1a144c8a83e5cc7fc1ca934de580f578947951
Emiliano Olivier <emiliano.olivier@maticmind.it>
2021-12-23 15:56:47 +0100

    dd some express features

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+
+
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('res send to express js and con la matregnas de token'));
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+//add the router
+app.use('/', router);
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
